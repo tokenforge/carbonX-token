2034a5eff2c5633c6aff93410e3b04df94541304
Hagen Hübel <hhuebel@itinance.com>
2022-07-03 09:25:32 +0200

    CarbonVault does not necessarily be a ERC1155 contract

--- contracts/CarbonVault.sol
+++ contracts/CarbonVault.sol
@@ -7,16 +7,14 @@ pragma solidity >=0.8.3;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
-
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
+
 import "./CarbonReceipt.sol";
 import "./CarbonX.sol";
 import "./ICarbonX.sol";
 
-contract CarbonVault is ERC1155, ERC1155Supply, ERC1155Receiver, Ownable {
+contract CarbonVault is ERC165, ERC1155Receiver, Ownable {
     using ECDSA for bytes32;
 
     mapping(uint256 => string) private _tokenUris;
@@ -26,12 +24,12 @@ contract CarbonVault is ERC1155, ERC1155Supply, ERC1155Receiver, Ownable {
 
     event SignerChanged(address indexed oldSigner, address indexed _signer);
 
-    modifier tokenExists(uint256 tokenId) {
+    /*modifier tokenExists(uint256 tokenId) {
         require(exists(tokenId));
         _;
-    }
+    }*/
 
-    constructor(CarbonReceipt receiptToken_, ICarbonX supportedToken, string memory baseUri_) ERC1155(baseUri_) {
+    constructor(CarbonReceipt receiptToken_, ICarbonX supportedToken) {
         _receiptToken = receiptToken_;
         addSupportedToken(supportedToken);
     }
@@ -46,47 +44,15 @@ contract CarbonVault is ERC1155, ERC1155Supply, ERC1155Receiver, Ownable {
     
     function tokenIsSupported(address token) public view returns (bool) {
         return _supportedTokens[token];
-    } 
-    
-    /**
-     * @dev Hook that is called before any token transfer. This includes minting
-     * and burning, as well as batched variants.
-     *
-     * The same hook is called on both single and batched variants. For single
-     * transfers, the length of the `id` and `amount` arrays will be 1.
-     *
-     * Calling conditions (for each `id` and `amount` pair):
-     *
-     * - When `from` and `to` are both non-zero, `amount` of ``from``'s tokens
-     * of token type `id` will be  transferred to `to`.
-     * - When `from` is zero, `amount` tokens of token type `id` will be minted
-     * for `to`.
-     * - when `to` is zero, `amount` of ``from``'s tokens of token type `id`
-     * will be burned.
-     * - `from` and `to` are never both zero.
-     * - `ids` and `amounts` have the same, non-zero length.
-     *
-     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
-     */
-    function _beforeTokenTransfer(
-        address, /*operator*/
-        address, /*from*/
-        address, /*to*/
-        uint256[] memory, /*ids*/
-        uint256[] memory, /*amounts*/
-        bytes memory /*data*/
-    ) internal virtual override(ERC1155, ERC1155Supply) {
-        revert("Not allowed");
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override(ERC1155, ERC1155Receiver)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165, ERC1155Receiver) returns (bool) {
+        return
+        interfaceId == type(ERC1155Receiver).interfaceId ||
+        super.supportsInterface(interfaceId);
     }
 
     function onERC1155Received(
